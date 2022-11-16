84c7132dc9140c61cbf1cdef3627fca9c643d722
liaohui-2002 <lh603143764@163.com>
2022-11-16 10:54:10 +0800

    构造二叉树--还要多看看

--- algorithm/src/com/liaohui/all/ex_tree/ex_前序中序构造二叉树_105/Solution.java
+++ algorithm/src/com/liaohui/all/ex_tree/ex_前序中序构造二叉树_105/Solution.java
@@ -1,6 +1,7 @@
 package com.liaohui.all.ex_tree.ex_前序中序构造二叉树_105;
 
 import com.liaohui.commom.TreeNode;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,16 +13,23 @@ import java.util.Map;
 public class Solution {
     private Map<Integer, Integer> indexMap;
 
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
     public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
         if (preorder_left > preorder_right) {
             return null;
         }
-
         // 前序遍历中的第一个节点就是根节点
         int preorder_root = preorder_left;
         // 在中序遍历中定位根节点
         int inorder_root = indexMap.get(preorder[preorder_root]);
-
         // 先把根节点建立出来
         TreeNode root = new TreeNode(preorder[preorder_root]);
         // 得到左子树中的节点数目
@@ -34,14 +42,9 @@ public class Solution {
         root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
         return root;
     }
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        // 构造哈希映射，帮助我们快速定位根节点
-        indexMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    @Test
+    public void test(){
+        int[] preorder = new int[]{3,9,20,15,7};
+        int[] inorder = new int[]{9,3,15,20,7};
     }
 }
